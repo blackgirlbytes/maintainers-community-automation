a829601497c0c752d92f3672bd6553da2067f98f
Rizel Scarlett <blackgirlbytes@github.com>
2022-12-22 13:42:16 -0500

    adding collaborator

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,17 +1,31 @@
 import { Probot } from 'probot';
 
 function bot(app: Probot) {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+// if issue is labeled, 'approved', invite the author to the repo
+  app.on('issues.labeled', async context => {
+    const { payload } = context;
+    const { issue, repository, label } = payload;
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    if (label.name === 'approved') {
+      const { data: repo } = await context.github.repos.get(context.repo());
+      const { data: user } = await context.github.users.getForUser({
+        username: issue.user.login,
+      });
+
+      await context.github.repos.addCollaborator({
+        owner: repository.owner.login,
+        repo: repository.name,
+        username: issue.user.login,        
+        permission: 'push',
+      });
+
+      await context.github.issues.createComment(
+        context.issue({
+          body: `@${issue.user.login} invited to the repo`,
+        })
+      );
+    }
+  });
 }
 
 export default bot;
